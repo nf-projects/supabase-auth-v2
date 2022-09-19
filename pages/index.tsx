f7eb9a73be86181a7a6c73fdd3d1e74923361fa1
nf-projects <nf-projects@users.noreply.github.com>
2022-09-18 22:30:18 -0700

    add basic homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Hero from "../components/landing/Hero";
+import Navbar from "../components/landing/Navbar";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
     <div>
-      
+      <Navbar />
+      <Hero />
     </div>
   );
 };
