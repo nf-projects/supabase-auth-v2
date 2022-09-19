c892695fa0bd8abc7f7bf457a2345d06ce83c867
nf-projects <nf-projects@users.noreply.github.com>
2022-09-18 22:44:07 -0700

    Update landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import FeatureA from "../components/landing/FeatureA";
+import FeatureB from "../components/landing/FeatureB";
+import FeatureC from "../components/landing/FeatureC";
+import Footer from "../components/landing/Footer";
 import Hero from "../components/landing/Hero";
 import Navbar from "../components/landing/Navbar";
+import ThreeTierPricing from "../components/landing/Pricing";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
@@ -10,6 +15,11 @@ const Home: NextPage = () => {
     <div>
       <Navbar />
       <Hero />
+      <FeatureA />
+      <FeatureB />
+      <FeatureC />
+      <ThreeTierPricing />
+      <Footer />
     </div>
   );
 };
