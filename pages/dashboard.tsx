ee690364dc61bc57f26c15ee958e6fed51e06c49
Spectre <nf-projects@users.noreply.github.com>
2022-09-19 10:03:05 -0700

    add auth logic to dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import { supabase } from "../utils/SupabaseClient";
+import { User } from "@supabase/supabase-js";
 
-const dashboard = () => {
-  return (
-    <div>dashboard</div>
-  )
-}
+const Dashboard = () => {
+  const router = useRouter();
+  const [user, setUser] = useState<User | null>();
 
-export default dashboard
+  useEffect(() => {
+    const getProfile = () => {
+      const profile = supabase.auth.user();
+
+      if (profile) {
+        setUser(profile);
+      } else {
+        router.push("/signin");
+      }
+    };
+
+    getProfile();
+  }, []);
+
+  if (!user) {
+    // Currently loading asynchronously User Supabase Information
+    return null;
+  }
+
+  return <div>dashboard</div>;
+};
+
+export default Dashboard;
