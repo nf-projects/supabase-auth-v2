828fdea91bb3a00ae269bb1b204e0942161915f8
nf-projects <nf-projects@users.noreply.github.com>
2022-09-18 20:41:18 -0700

    signup

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,6 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-import { supabase } from "../utils/SupabaseClient.js";
-
 const Home: NextPage = () => {
   return (
     <div>
---
+++ pages/signup.tsx
@@ -0,0 +1,140 @@
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  HStack,
+  InputRightElement,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+  Link,
+} from "@chakra-ui/react";
+
+import { MouseEventHandler, useState } from "react";
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
+
+import { supabase } from "../utils/SupabaseClient";
+
+import { useRouter } from "next/router";
+
+export default function SignUp() {
+  const router = useRouter();
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+
+    if (error) {
+      alert(JSON.stringify(error));
+    } else {
+      router.push("/signin");
+    }
+  };
+
+  const handleSubmitWithGitHub = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const { error } = await supabase.auth.signIn(
+      {
+        provider: "github",
+      },
+      {
+        redirectTo: "http://localhost:3000/callback/",
+      }
+    );
+
+    if (error) {
+      alert(JSON.stringify(error));
+    }
+  };
+
+  return (
+    <Flex
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+        <Stack align={"center"}>
+          <Heading fontSize={"4xl"} textAlign={"center"}>
+            Welcome!
+          </Heading>
+          <Text fontSize={"lg"} color={"gray.600"}>
+            Sign up to get started 👏
+          </Text>
+        </Stack>
+        <Box
+          rounded={"lg"}
+          bg={useColorModeValue("white", "gray.700")}
+          boxShadow={"lg"}
+          p={8}
+        >
+          <Stack spacing={4}>
+            <FormControl id="email" isRequired>
+              <FormLabel>Email address</FormLabel>
+              <Input
+               type="email" 
+               value={email}
+               onChange={(e) => setEmail(e.target.value)}  
+               />
+            </FormControl>
+            <FormControl id="password" isRequired>
+              <FormLabel>Password</FormLabel>
+              <InputGroup>
+                <Input
+                 type={showPassword ? "text" : "password"} 
+                 value={password}
+                 onChange={(e) => setPassword(e.target.value)}
+                 />
+                <InputRightElement h={"full"}>
+                  <Button
+                    variant={"ghost"}
+                    onClick={() =>
+                      setShowPassword((showPassword) => !showPassword)
+                    }
+                  >
+                    {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+            </FormControl>
+            <Stack spacing={10} pt={2}>
+              <Button
+                loadingText="Submitting"
+                size="lg"
+                bg={"blue.400"}
+                bgGradient="linear(to-r, red.400,pink.400)"
+                color={"white"}
+                _hover={{
+                  bg: "blue.500",
+                }}
+                onClick={handleSubmit}
+              >
+                Sign Up
+              </Button>
+            </Stack>
+            <Stack pt={6}>
+              <Text align={"center"}>
+                Already a user? <Link color={"blue.400"}>Login</Link>
+              </Text>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
+}
