146975d2bf87ad23b8979448928c0107211f8e6a
nf-projects <nf-projects@users.noreply.github.com>
2022-09-18 19:06:24 -0700

    fix bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,17 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
-import { supabase } from '../utils/SupabaseClient.js';
+import { supabase } from "../utils/SupabaseClient.js";
 
 const Home: NextPage = () => {
   return (
-    
-  )
-}
+    <div>
+      <h1>Test</h1>
+      <h2>Hi</h2>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
