f201d98ef1876afe1ca587bf5f8fd820fda67b8f
nf-projects <nf-projects@users.noreply.github.com>
2022-09-19 19:20:50 -0700

    Update dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import { supabase } from "../utils/SupabaseClient";
@@ -6,6 +7,7 @@ import { User } from "@supabase/supabase-js";
 const Dashboard = () => {
   const router = useRouter();
   const [user, setUser] = useState<User | null>();
+  const [email, setEmail] = useState();
 
   useEffect(() => {
     const getProfile = () => {
@@ -26,7 +28,25 @@ const Dashboard = () => {
     return null;
   }
 
-  return <div>dashboard</div>;
+  const handleSignOut = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const { error } = await supabase.auth.signOut();
+
+    if (error) {
+      alert("An error occured while signing out: " + JSON.stringify(error));
+    }
+    router.push("/");
+  };
+
+  return (
+    <div>
+      <h1>Welcome 👋</h1>
+      <p>Your email is {user.email}</p>
+
+      <button onClick={handleSignOut}>Sign Out</button>
+    </div>
+  );
 };
 
 export default Dashboard;
