85d507b2e76da3341176eb3080f491a7c27e55f8
nf-projects <nf-projects@users.noreply.github.com>
2022-09-24 15:33:43 -0700

    Update sign in to include a button

--- pages/new-password-success.tsx
+++ pages/new-password-success.tsx
@@ -2,16 +2,37 @@ import React from 'react'
 import Success from '../components/Success'
 import SignIn from './signin'
 
+import { useRouter } from "next/router";
+
+import { Button, Center } from "@chakra-ui/react";
+
 const NewPasswordSuccess = () => {
+  const router = useRouter();
+
+  const sendToSignIn = async () => {
+    router.push("/signin")
+  }
+
   return (
     <div>
-        <Success
-         heading="Password Reset Successfully"
-         text="Sign in with your new password:"
-         />
-         <SignIn />
+      <Success
+        heading="Password Reset Successfully"
+        text="Click to sign in with your new password!"
+      />
+      <Center>
+        <Button
+          bg={"blue.400"}
+          color={"white"}
+          _hover={{
+            bg: "blue.500",
+          }}
+          onClick={sendToSignIn}
+        >
+          Sign in
+        </Button>
+      </Center>
     </div>
-  )
+  );
 }
 
 export default NewPasswordSuccess
